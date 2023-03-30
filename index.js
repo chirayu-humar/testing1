589ba19dbcaa7706b71ae54efa1910d88e67cd00
chirayu-humar <chirayuhumar@gmail.com>
2023-03-30 11:26:47 +0000

    fourth commit

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ let addDays = require("date-fns/addDays");
 
 function newDate(days) {
   const result = addDays(new Date("2020-07-22"), days);
-  let temp = `${result.getDate()}-${result.getMonth()}-${result.getFullYear()}`;
+  let temp = `${result.getDate()}-${
+    result.getMonth() + 1
+  }-${result.getFullYear()}`;
   return temp;
 }
 
